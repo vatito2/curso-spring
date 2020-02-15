bfc8cacb0531f9108624a16b84cb97ac0b7befd6
Emmanuel Gomez <vatito2@hotmail.com>
2020-02-15 11:56:29 -0600

    Ejemplo para volver al pasado

--- EjemploGit/src/org/emmanuel/curso/Emmanuel.java
+++ EjemploGit/src/org/emmanuel/curso/Emmanuel.java
@@ -5,6 +5,7 @@ public class Emmanuel {
 	public static void main(String... args)
 	{
 		System.out.println("Daddy Emmanuel was here");
+		System.out.println("Daddy Emmanuel was here again");
 	}
 
 }

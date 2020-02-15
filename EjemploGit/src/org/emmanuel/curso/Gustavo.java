e87eb95c36f306b938ca9ba605bcb899cdac07d7
tonaVM <tonathiuviveros@gmail.com>
2020-02-15 11:16:59 -0600

    insulto al de la izquierda

--- EjemploGit/src/org/emmanuel/curso/Gustavo.java
+++ EjemploGit/src/org/emmanuel/curso/Gustavo.java
@@ -4,8 +4,8 @@ public class Gustavo {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Marino se la come");
-
+		System.out.println("Marino se la come doblada y entera");
+		System.out.println("El david es una puerca y se la come aunque no tenga permiso");
 	}
 
 }

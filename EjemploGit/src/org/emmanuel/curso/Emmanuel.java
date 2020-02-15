089e765b9fbf992933ddf670c67c16b802bc6bc7
tonaVM <tonathiuviveros@gmail.com>
2020-02-15 12:05:24 -0600

    borre el cambio del profe

--- EjemploGit/src/org/emmanuel/curso/Emmanuel.java
+++ EjemploGit/src/org/emmanuel/curso/Emmanuel.java
@@ -5,7 +5,6 @@ public class Emmanuel {
 	public static void main(String... args)
 	{
 		System.out.println("Daddy Emmanuel was here");
-		System.out.println("Daddy Emmanuel was here again");
 	}
 
 }

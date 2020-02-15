d4ebff58a5d81998e9fd1b54db5cef79f80e054e
tonaVM <tonathiuviveros@gmail.com>
2020-02-15 10:48:08 -0600

    puse un mensaje verdadero acerca de los gustosjean

---
+++ EjemploGit/src/org/emmanuel/curso/Tona.java
@@ -0,0 +1,10 @@
+package org.emmanuel.curso;
+
+public class Tona {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("jean carlo se la come enterita");
+	}
+
+}
